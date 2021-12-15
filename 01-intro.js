4847676f27853a718f1837f3c2869adf5fe4734e
Pasindu Lakshan <74607482+pasindulakshan@users.noreply.github.com>
2021-12-15 17:50:46 +0530

    Create 01-intro.js

---
+++ 01-intro.js
@@ -0,0 +1,10 @@
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+console.log(`it's my first node app!!`);
